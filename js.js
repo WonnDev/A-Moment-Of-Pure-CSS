c1c308ff6ad10694a3399c9f4ed112e622428201
Wonn <wonndev@gmail.com>
2022-09-27 11:54:03 +0700

    CSSPure

---
+++ js.js
@@ -0,0 +1,10 @@
+// A moment of pure CSS
+
+// No images - No JavaScript - No Videos - Just CSS (& HTML)
+
+// How it was made:
+// youtube.com/watch?v=mr_Oxr0JqAQ
+
+// linktr.ee/ivorjetski for more
+
+// Nothing to show
